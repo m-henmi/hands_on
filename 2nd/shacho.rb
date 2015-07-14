d592ea3ad626080537c0db08729e82212ecce051
m-henmi <m.henmi@gmail.com>
2015-07-14 19:00:25 +0900

    回数ごとのディレクトリを作成

--- 2nd/shacho.rb
+++ 2nd/shacho.rb
@@ -3,6 +3,7 @@ require_relative 'shain'
 require_relative 'tanto'
 require_relative 'shunin'
 require_relative 'bucho'
+require_relative 'salary'
 
 shain = Tanto.new
 shain.standup
@@ -12,6 +13,6 @@ shain2 = Shunin.new
 shain2.standup
 puts "私の給料は#{shain2.calclate_salary(100)}円です。"
 
-shain3 = Bucho.new
+shain3 = Bucho.new(100)
 shain3.standup
-puts "私の給料は#{shain3.calclate_salary(100)}円です。"
+puts "私の給料は#{shain3.calclate_salary()}円です。"
--- 2nd/shain.rb
+++ 2nd/shain.rb
@@ -1,5 +1,12 @@
 # coding: utf-8
+require_relative 'salary'
+
 class Shain
+include Salary
+  def initialize(kihonkyu)
+    @kihonkyu = kihonkyu
+  end
+  
   def standup
     puts "社員はとりあえず起立する"
   end
